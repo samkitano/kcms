21874ed56fa8c179b7b23214490de6f1c184d6ce
Sam Kitano <sam.kitano@gmail.com>
2018-01-25 20:13:07 +0000

    fixed code style issues

--- resources/assets/js/admin/php/modules/_settings-alerts.js
+++ resources/assets/js/admin/php/modules/_settings-alerts.js
@@ -1,7 +1,8 @@
+/* global $ */
 import { getHidden, delHidden, resetHidden } from './_local-storage'
 import { translate } from '../helpers'
 
-let addHiddenElement = (txt, link) => `<p class="p-2 text-sm flex justify-between"><span>${txt}</span> ${addUnhideButton(link)}</p>`
+let addHiddenElement = (txt, link) => `<p class="py-2 text-sm flex justify-between"><span>${txt}</span> ${addUnhideButton(link)}</p>`
 
 let addUnhideButton = (link) => `<a
     class="unhide-alert self-center text-xs ml-1 no-outline px-1 py-1 leading-none border rounded hover:text-blue-darker hover:bg-blue-lightest"
@@ -13,13 +14,10 @@ let addUnhideAllButton = () => `<a class="inline-block unhide-alert-all text-sm 
 
 let fillSettings = () => {
   let $alertSettingsContainer = $('.settings_hidden-alerts')
-
-  if (!$alertSettingsContainer) return false
-
   let store = getHidden()
 
   if (store === null || store.length === 0) {
-    $alertSettingsContainer.html(`<p class="p-4 text-lg">${translate('alerts.no_hidden_alerts')}</p>`)
+    $alertSettingsContainer.html(`<p class="text-lg">${translate('alerts.no_hidden_alerts')}</p>`)
 
     return false
   }
