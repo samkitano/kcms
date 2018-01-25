f6bed8d12dbd5c4448edcc494e212a72d025e601
Sam Kitano <sam.kitano@gmail.com>
2018-01-25 20:11:53 +0000

    added clear cache feature

---
+++ resources/assets/js/admin/php/modules/_settings-clear-cache.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import swal from 'sweetalert2'
+import { alertSystemError, translate } from '../helpers'
+
+$(document).on('click', '.clear-cache', function (e) {
+  e.preventDefault()
+
+  axios
+    .post('/admin/settings/clearcache', { _method: 'POST' })
+    .then((r) => {
+      swal({ title: translate('alerts.success'), html: translate('alerts.cache_cleared'), type: 'success' })
+    })
+    .catch((e) => {
+      alertSystemError(e)
+    })
+})
