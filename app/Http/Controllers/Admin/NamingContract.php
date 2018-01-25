91c6b04a2c1e26f5ea96d6db9007a8b93b53db7e
Sam Kitano <sam.kitano@gmail.com>
2018-01-25 22:41:58 +0000

    Created contract for resource naming at controller level

---
+++ app/Http/Controllers/Admin/NamingContract.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+interface NamingContract
+{
+    /**
+     * Return the Menu group for the resource
+     *
+     * @return string
+     */
+    public static function getMenuGroup(): string;
+
+    /**
+     * Return the resource name to be used as page title.
+     *
+     * @param bool $singular
+     * @return string
+     */
+    public static function getTitle($singular = false): string;
+}
