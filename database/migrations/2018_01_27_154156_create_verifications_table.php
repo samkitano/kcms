e58d24c4d84ccb8e19717074a463ebaba151cf40
Sam Kitano <sam.kitano@gmail.com>
2018-01-27 19:35:16 +0000

    implemented user email verification

---
+++ database/migrations/2018_01_27_154156_create_verifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verifications', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')
+                  ->index()
+                  ->unsigned();
+
+            $table->string('token', 64)
+                  ->index();
+
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verifications');
+    }
+}
