47f6924ebaa677c83ed68f49dba81ab6c6ecb7d9
Sam Kitano <sam.kitano@gmail.com>
2018-01-27 19:33:43 +0000

    implemented user email verification

---
+++ app/Kcms/Services/Auth/Users/VerifiesUsers.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Kcms\Services\Auth\Users;
+
+use Mail;
+use Carbon\Carbon;
+use App\Kcms\Mail\Users\ActivateAccount;
+
+trait VerifiesUsers
+{
+    public static function makeVerification($user)
+    {
+        if (! config('kcms.user_verification')) {
+            return null;
+        }
+
+        $token = static::generateToken();
+
+        \DB::table('verifications')->insert(
+            [
+                'user_id' => $user->id,
+                'token' => $token,
+                'created_at' => Carbon::now(),
+            ]
+        );
+
+        Mail::to($user->email)->send(new ActivateAccount($user, $token));
+    }
+
+    public static function verify($token)
+    {
+        $v = \DB::table('verifications')
+                ->where('token', '=', $token)
+                ->first();
+
+        if (empty($v)) {
+            return __('auth.invalid_token');
+        }
+
+        // TODO: check expiration
+
+        $user = User::find($v->user_id);
+
+        \DB::table('verifications')
+           ->where('id', '=', $v->id)
+           ->delete();
+
+        $user->verify()->registerLastActivity()->save();
+
+        return $user;
+    }
+
+    protected static function generateToken()
+    {
+        return hash_hmac('sha256', str_random(40), config('app.key'));
+    }
+}
