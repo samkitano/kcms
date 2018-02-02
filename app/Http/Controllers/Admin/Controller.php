c2790a5166f397f2e817dbf7693607b9da00f5f3
Sam Kitano <sam.kitano@gmail.com>
2018-02-02 10:55:24 +0000

    added todo

--- app/Http/Controllers/Admin/Controller.php
+++ app/Http/Controllers/Admin/Controller.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers\Admin;
 
-use ReflectionClass;
+//use ReflectionClass;
 //use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
+//use Illuminate\Support\Collection;
 //use Illuminate\Support\Facades\Cache;
 //use App\Models\Scopes\NonDraftScope;
 //use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers\Admin
+ * @TODO: cleanup
+ */
 abstract class Controller
 {
     use ValidatesRequests;
 
     /** @var string */
     protected $moduleName;
-    protected $modelClass;
-    protected $modelName;
+//    protected $modelClass;
+//    protected $modelName;
 
     /** @var bool */
     protected $redirectToIndex = false;
@@ -25,28 +30,28 @@ abstract class Controller
     public function __construct()
     {
         $this->moduleName = $this->moduleName();
-        $this->modelClass = $this->modelClass();
-        $this->modelName = __("admin.models.{$this->moduleName}");
+//        $this->modelClass = $this->modelClass();
+//        $this->modelName = __("admin.models.{$this->moduleName}");
     }
 
-    public function index()
-    {
-        $models = $this->all();
-
-        return view("admin.{$this->moduleName}.index", compact('models'));
-    }
-
-    protected function all(): Collection
-    {
-        return call_user_func([$this->modelClass, 'all']);
-    }
-
-    protected function modelClass(): string
-    {
-        return (new ReflectionClass($this))
-            ->getMethod('make')
-            ->getReturnType();
-    }
+//    public function index()
+//    {
+//        $models = $this->all();
+//
+//        return view("admin.{$this->moduleName}.index", compact('models'));
+//    }
+//
+//    protected function all(): Collection
+//    {
+//        return call_user_func([$this->modelClass, 'all']);
+//    }
+//
+//    protected function modelClass(): string
+//    {
+//        return (new ReflectionClass($this))
+//            ->getMethod('make')
+//            ->getReturnType();
+//    }
 
     protected function moduleName(): string
     {
