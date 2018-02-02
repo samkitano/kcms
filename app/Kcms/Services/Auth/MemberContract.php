2e4e2a05b9f206e23cfd14c7775e7d34d81462b3
Sam Kitano <sam.kitano@gmail.com>
2018-02-02 10:43:51 +0000

    doc blocks

--- app/Kcms/Services/Auth/MemberContract.php
+++ app/Kcms/Services/Auth/MemberContract.php
@@ -4,6 +4,19 @@ namespace App\Kcms\Services\Auth;
 
 interface MemberContract
 {
+
+    /**
+     * The fields to present in index view
+     *
+     * @return array
+     */
     public static function presentable(): array;
+
+    /**
+     * The fields that can be created/edited
+     *
+     * @param null $id
+     * @return array
+     */
     public static function editable($id = null): array;
 }
