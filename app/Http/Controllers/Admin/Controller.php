9292581c7c045d0448bf063f8b0ddb17494551e4
Sam Kitano <sam.kitano@gmail.com>
2018-02-02 18:39:11 +0000

    cleanup

--- app/Http/Controllers/Admin/Controller.php
+++ app/Http/Controllers/Admin/Controller.php
@@ -2,57 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
-//use ReflectionClass;
-//use Illuminate\Http\Request;
-//use Illuminate\Support\Collection;
-//use Illuminate\Support\Facades\Cache;
-//use App\Models\Scopes\NonDraftScope;
-//use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-/**
- * Class Controller
- * @package App\Http\Controllers\Admin
- * @TODO: cleanup
- */
 abstract class Controller
 {
     use ValidatesRequests;
 
     /** @var string */
     protected $moduleName;
-//    protected $modelClass;
-//    protected $modelName;
 
     /** @var bool */
     protected $redirectToIndex = false;
 
+    /**
+     * Controller constructor.
+     */
     public function __construct()
     {
         $this->moduleName = $this->moduleName();
-//        $this->modelClass = $this->modelClass();
-//        $this->modelName = __("admin.models.{$this->moduleName}");
     }
 
-//    public function index()
-//    {
-//        $models = $this->all();
-//
-//        return view("admin.{$this->moduleName}.index", compact('models'));
-//    }
-//
-//    protected function all(): Collection
-//    {
-//        return call_user_func([$this->modelClass, 'all']);
-//    }
-//
-//    protected function modelClass(): string
-//    {
-//        return (new ReflectionClass($this))
-//            ->getMethod('make')
-//            ->getReturnType();
-//    }
-
+    /**
+     * @return string
+     */
     protected function moduleName(): string
     {
         return lcfirst(str_replace_last('Controller', '', class_basename($this)));
