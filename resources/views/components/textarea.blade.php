d1140765d358246ace9fe4427dd1c289a034dfee
Sam Kitano <sam.kitano@gmail.com>
2018-03-05 23:01:59 +0000

    fixed default val

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,5 @@
 <label for="{{ $name }}" class="label">{{ $field['label'] }}</label>
 
 <textarea class="input has-editor" name="{{ $name }}" id="{{ $name }}" cols="30" rows="10">
-    {{ $field['value'] }}
+    {{ $old ?? $field['value'] }}
 </textarea>
