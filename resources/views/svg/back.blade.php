75f266faa929048239153ace563cfe220676a931
Sam Kitano <sam.kitano@gmail.com>
2018-03-07 14:11:35 +0000

    first commit

---
+++ resources/views/svg/back.blade.php
@@ -0,0 +1,7 @@
+<svg class="fill-current"
+     width="{{ $width ?? 16 }}"
+     height="{{ $height ?? 16 }}"
+     role="img"
+     viewBox="0 0 20 20"
+     xmlns="http://www.w3.org/2000/svg"><polygon points="3.828 9 9.899 2.929 8.485 1.515 0 10 .707 10.707 8.485 18.485 9.899 17.071 3.828 11 20 11 20 9 3.828 9"/>
+    <title>{{ __t('buttons.back_idx') }}</title></svg>
